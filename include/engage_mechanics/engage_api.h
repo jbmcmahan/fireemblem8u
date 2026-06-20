d1adbe085e20e4d3424c1ae79ecc1415e0cdcd2f
Jeff McMahan <jbmcmahan@gmail.com>
2026-06-20 06:41:18 -0400

    Revert "engage: skills TDD — struct SkillDef header + tests (#46) (#55)"
    This reverts commit 345b51344720eeb7020abad6e40fb8f8ca801e9a.

--- include/engage_mechanics/engage_api.h
+++ include/engage_mechanics/engage_api.h
@@ -4,6 +4,5 @@
 #include "engage_mechanics/engage_config.h"
 #include "engage_mechanics/engage_meter.h"
 #include "engage_mechanics/engage_data.h"
-#include "engage_mechanics/engage_skills.h"
 
 #endif // GUARD_ENGAGE_MECHANICS_ENGAGE_API_H
